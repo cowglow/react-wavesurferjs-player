adbff44804189372990438db6507443a3dbcec60
Philip Saa <cowglow@gmail.com>
2024-12-20 10:15:05 +0100

    Typescript improvements

--- src/feature/controls/TimestampDisplay.tsx
+++ src/feature/controls/TimestampDisplay.tsx
@@ -1,16 +1,19 @@
 import {Typography} from "@mui/material";
 
+const formatOptions: Intl.DateTimeFormatOptions = {
+    hour12: false,
+    minute: '2-digit',
+    second: '2-digit',
+}
+
 export default function TimestampDisplay({timestamp}: { timestamp: number }) {
-    const date = new Date(timestamp - 60 * 60 * 1000)
-    const formattedTimestamp = date.toLocaleTimeString("en-US", {
-        hour12: false,
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-    })
+    const date = new Date(timestamp * 1000)
+    const hours = (date.getHours() - 1).toString().padStart(2, '0')
+    const formattedTimestamp = date.toLocaleTimeString("en-US", formatOptions)
+    const milliseconds = new Date(timestamp * 1000).getMilliseconds().toString().padStart(3, '0')
     return (
         <Typography variant="h3">
-            {`${formattedTimestamp}:${date.getMilliseconds().toString().padStart(3, '0')}`}
+            {`${hours}:${formattedTimestamp}:${milliseconds}`}
         </Typography>
     )
 }
